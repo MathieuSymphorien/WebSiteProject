5c70eb0dbe944aaef1f6532eebbb08dea4f17777
MathieuSymphorien <72963501+MathieuSymphorien@users.noreply.github.com>
2024-07-04 11:22:38 +0200

    feat: component todelete2

---
+++ src/app/todelete2/todelete2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todelete2',
+  standalone: true,
+  imports: [],
+  templateUrl: './todelete2.component.html',
+  styleUrl: './todelete2.component.scss'
+})
+export class Todelete2Component {
+
+}
